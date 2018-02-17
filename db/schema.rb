9c9bd79782304b42b5f8bff99751350aa9720ca8
Natsuko Robyn Shinozaki <naztshinozaki@gmail.com>
2018-02-17 15:31:51 +0900

    add model and controllers for posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180217061515) do
+ActiveRecord::Schema.define(version: 20180217062401) do
+
+  create_table "posts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "photo"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
